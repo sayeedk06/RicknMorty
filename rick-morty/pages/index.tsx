fc7aabc2da519c597ab9e1a4b83eceb6f43b6e63
sayeedk06 <sayeed.k06@gmail.com>
2023-01-31 18:47:04 +0600

    added query search using onkeydown event

--- rick-morty/pages/index.tsx
+++ rick-morty/pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/Home.module.css'
 import { GetServerSideProps } from 'next'
 import Card from '../components/Card'
 import Pagination from '@/components/Pagination'
-import { useEffect, useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -14,9 +14,16 @@ import { useRouter } from 'next/router'
 
 export default function Home({characters, infos}:any) {
   const [currentPage, setCurrentPage] = useState(1)
+  const [query, setQuery] = useState('')
   const router = useRouter()
 
-  const buttonHandler = (e:any) => router.push('/?page=' + e.target.value)
+  const buttonHandler = (e:any) => router.push('/?page=' + e.target.value);
+  const putQuery = (e:any) => setQuery(e.target.value);
+  const searchQuery = (e:any) => {
+    if (e.key ==='Enter'){
+      // console.log(query)
+      router.push('/?name=' + query);
+    }}
 
   return (
     <>
@@ -27,7 +34,7 @@ export default function Home({characters, infos}:any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <input className={styles.searchBox} type="text" placeholder='Search for characters here..........'/>
+        <input className={styles.searchBox} type="text" onChange={putQuery} onKeyDown={searchQuery} placeholder='Search for characters here..........'/>
         <div className={styles.flexContainer}>
         {characters.map(
                 (character:any)=>{
@@ -42,7 +49,7 @@ export default function Home({characters, infos}:any) {
                   )}
         </div>
         <div>
-          <Pagination totalPage={infos.pages} currentPage={currentPage} buttonHandler={buttonHandler}/>
+          <Pagination key={infos.count} totalPage={infos.pages} currentPage={currentPage} buttonHandler={buttonHandler}/>
       </div>
       </main>
       
@@ -53,12 +60,18 @@ export default function Home({characters, infos}:any) {
 
 
 export const getServerSideProps : GetServerSideProps = async (context:any) => {
-  const page = context.query.page
-  const url = 'https://rickandmortyapi.com/api/character/?page=' + String(page)
-  // const response = await fetch(url + '?' + new URLSearchParams({'page': curre}))
+  
+  var page = context.query.page
+  var url = 'https://rickandmortyapi.com/api/character/?page=' + String(page)
+  
+  // console.log(context.query.page)
+  if (context.query.name) {
+    page = context.query.name
+    url = 'https://rickandmortyapi.com/api/character/?name=' + String(page)
+  }
   const response = await fetch(url)
   const data = await response.json()
-  // console.log(context.query.page)
+  console.log(data.info)
   return {
     props : {
       characters : data.results,
